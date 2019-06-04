8c2376bcd70fb3298e9b1c096210fbf28521d207
claudineiafs <claudineiafs@gmail.com>
2019-06-04 20:51:31 -0300

    criado classe de controle de produtos

---
+++ produtos/src/main/java/org/zero2one/produtos/controller/ProductsController.java
@@ -0,0 +1,15 @@
+package org.zero2one.produtos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @GetMapping
+    public String get() { return "OK"; }
+
+}
+
