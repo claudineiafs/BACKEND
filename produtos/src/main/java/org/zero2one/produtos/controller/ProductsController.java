04b6eabfa1bfff1f91a4a50a4b9568bc54d12842
claudineiafs <claudineiafs@gmail.com>
2019-06-06 08:15:33 -0300

    criado classe de controle de produtos

--- produtos/src/main/java/org/zero2one/produtos/controller/ProductsController.java
+++ produtos/src/main/java/org/zero2one/produtos/controller/ProductsController.java
@@ -1,15 +1,42 @@
 package org.zero2one.produtos.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zero2one.produtos.model.Product;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
 
+    private Map<String, Product> products = new HashMap<>();
+
+    public ProductsController() {
+        Product celular = new Product();
+        celular.setId("1A");
+        celular.setDescription("Novo Moto X 2");
+        celular.setName("Moto X");
+        celular.setPrice(1999.99);
+
+        Product notebook = new Product();
+        celular.setId("1B");
+        celular.setDescription("Dell i9");
+        celular.setName("Dell");
+        celular.setPrice(9999.99);
+
+        products.put(celular.getId(), celular);
+        products.put(notebook.getId(), notebook);
+    }
+
     @GetMapping
-    public String get() { return "OK"; }
+    public Collection<Product> get() {
+        return products.values();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable("id") String id){
+        return products.get(id);
+    }
 
 }
 
