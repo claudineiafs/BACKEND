548184fb9504451f8e80148e0790fcdf0bf85b01
claudineiafs <claudineiafs@gmail.com>
2019-06-10 21:37:15 -0300

    aula de swagger

--- produtos/src/main/java/org/zero2one/produtos/config/WebSecurityConfig.java
+++ produtos/src/main/java/org/zero2one/produtos/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package org.zero2one.produtos.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
